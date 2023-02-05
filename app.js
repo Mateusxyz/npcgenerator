da2004c23b57776db23217d69a55c91e66e5c379
Mateus Jardim <dteteus@gmail.com>
2023-02-04 21:20:30 -0300

    dp

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const router = require('./routes/routes');
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+//carregando router no app
 app.use("/", router);
+
+//expondo a pasta public
+app.use(express.static('public'))
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
