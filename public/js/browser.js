79a4d48ec2e37ae953be092f8f0e22f659223f64
Lucas Tavares <lucastavaresdev@protonmail.com>
2023-02-06 13:10:40 -0300

    Jquey dice rolls for damage

--- public/js/browser.js
+++ public/js/browser.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
+    $(document).on('click','.dice', function () {
+        let thisDice = $(this).data().roll 
+        let rolls = thisDice.split('d')
+        function rolld6() {
+            let total = 0;
+            for (let i = 0; i < rolls[0]; i++) {
+              total += Math.floor(Math.random() * 6) + 1;
+            }
+            console.log(total);
+          }
+          rolld6()
+    })
     var origin   = window.location.origin;
     console.log(origin)
     $("#generate").click(function () {
